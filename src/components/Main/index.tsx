aa830f8fdc3a6bf7eed59ce94c0d86ee7d9a54c9
Alexander Davis <alexander.davis.098@gmail.com>
2022-10-28 12:07:56 -0300

    Feat: Add styled-components to home page

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,17 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="img/logo.svg" alt="Atom - React Avançado escrito ao lado" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Ilustration
+      src="img/hero-illustration.svg"
+      alt="Desenvolvedor de frente para uma tela com código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
